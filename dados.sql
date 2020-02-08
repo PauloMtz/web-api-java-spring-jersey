b05725f9332fd508715c28e5e9a66e052863a385
PauloMtz <pramosmtz@gmail.com>
2020-02-08 16:16:55 -0300

    Ateração no banco de dados

--- dados.sql
+++ dados.sql
@@ -15,35 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `clientes`
---
-
-DROP TABLE IF EXISTS `clientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `clientes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(65) DEFAULT NULL,
-  `email` varchar(65) DEFAULT NULL,
-  `endereco` varchar(128) DEFAULT NULL,
-  `telefone` varchar(16) DEFAULT NULL,
-  `bairro` varchar(65) DEFAULT NULL,
-  `cep` varchar(16) DEFAULT NULL,
-  `cpf` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `clientes`
---
-
-LOCK TABLES `clientes` WRITE;
-/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (3,'Maria da Silva','maria@email.teste','Rua Teste A, 90B','(11) 2233-4455','Centro','11.222-333','123.123.123-12'),(4,'JosÃ© Teste','jose@email.teste','Rua 22 de Abril, 99A','(22) 5566-7788','BairrolÃ¢ndia','12.345-678','999.888.777-66'),(5,'Pedro Teste','pedro@email.teste','Rua Sem Nome, s/n','(33) 7788-9900','FundÃ£o','33.444-555','000.000.000-00'),(6,'Teste II','teste@email.teste','Rua Sei LÃ¡ o QuÃª, s/n','(11) 3344-5566','Sei LÃ¡','22.222-000','123.123.123-12'),(7,'Ana Maria Teste','ana@email.teste','Rua Teste III','(44) 5566-7788','Qualquer Coisa','22.222-000','987.654.432-00'),(8,'Dom Pedro III','dompedro@email.teste','Rua Brasil, 9999','(99) 9988-7766','Imperial','99.888-777','909.909.808-77'),(9,'Maria Antonieta','antonieta@email.teste','Rua Paris, 9999','(88) 7766-5544','Eiffel','99.777-888','888.888.777-66'),(11,'Teste IV','testeiv@email.teste','Rua Teste IV','(00) 0000-0000','Teste IV','00.000-000','000.000.000-00');
-/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `contatos`
@@ -53,10 +24,10 @@ DROP TABLE IF EXISTS `contatos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contatos` (
-  `id` varchar(10) NOT NULL,
+  `id` varchar(65) NOT NULL,
   `nome` varchar(65) NOT NULL,
   `email` varchar(65) DEFAULT NULL,
-  `cpf` varchar(11) DEFAULT NULL,
+  `cpf` varchar(14) DEFAULT NULL,
   `telefone` varchar(65) DEFAULT NULL,
   `data_nascimento` date DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -69,7 +40,7 @@ CREATE TABLE `contatos` (
 
 LOCK TABLES `contatos` WRITE;
 /*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
-INSERT INTO `contatos` VALUES ('1001','Pedro Teste','pedro@email.teste','11122233344','(00) 0001-0002','1999-10-25');
+INSERT INTO `contatos` VALUES ('82366214-8ca8-4612-a64f-79d1374f0681','Pedro Teste','pedro@email.teste','999.888.777-66','(00) 0001-0002','1993-11-11'),('957d8020-a27a-4c04-85ee-2e18698c63b1','Maria da Silva','maria.silva@email.teste','123.456.789-01','(00) 0011-0022','1987-03-12'),('c2ff67be-d973-409e-894b-8b15278ea629','Sebastião da Silva','sebastiao.silva@email.com','000.111.222-33','(00) 0011-0022 / (00) 1100-0022','2001-04-13'),('b3bd0450-e90f-4368-a30f-1cb7b9e2dba7','Ana Maria','ana.maria@email.com','999.777.555-33','(99) 9988-7766','2000-11-23');
 /*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,7 +52,7 @@ DROP TABLE IF EXISTS `enderecos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `enderecos` (
-  `id_contato` varchar(10) NOT NULL,
+  `id_contato` varchar(65) NOT NULL,
   `estado` varchar(65) DEFAULT NULL,
   `cidade` varchar(65) DEFAULT NULL,
   `bairro` varchar(65) DEFAULT NULL,
@@ -96,35 +67,9 @@ CREATE TABLE `enderecos` (
 
 LOCK TABLES `enderecos` WRITE;
 /*!40000 ALTER TABLE `enderecos` DISABLE KEYS */;
-INSERT INTO `enderecos` VALUES ('1001','Minas Gerais','Santana do Pirapama','Andradas','Rua Teste I, 100');
+INSERT INTO `enderecos` VALUES ('82366214-8ca8-4612-a64f-79d1374f0681','PA','Santarém','Boa Vista','Rua Teste IV, 104'),('957d8020-a27a-4c04-85ee-2e18698c63b1','SP','Marília','Bom Jardim','Rua Teste II, 101'),('c2ff67be-d973-409e-894b-8b15278ea629','GO','Testolândia','Serafim Cruz','Rua Teste III, 102'),('b3bd0450-e90f-4368-a30f-1cb7b9e2dba7','MS','Santa Cruz','Ermelinda','Rua Teste V, 105');
 /*!40000 ALTER TABLE `enderecos` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(65) DEFAULT NULL,
-  `email` varchar(65) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1004 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuarios`
---
-
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1002,'Pedro Teste','pedro@email.teste'),(1003,'Maria Rita da Silva','maria.rita@email.teste');
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -134,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-06 18:50:49
+-- Dump completed on 2020-02-08 16:14:18
